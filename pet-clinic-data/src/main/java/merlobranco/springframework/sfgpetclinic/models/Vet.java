bea59585d66c5d1be669df920aeea08c28f1c9ef
Brais Cidras <braisomary@gmail.com>
2021-03-28 20:24:00 +0200

    Adding List Vets. Closes #15

--- pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/models/Vet.java
@@ -2,4 +2,6 @@ package merlobranco.springframework.sfgpetclinic.models;
 
 public class Vet extends Person {
 
+	private static final long serialVersionUID = 405138796902956638L;
+
 }
--- pet-clinic-web/src/main/java/merlobranco/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/merlobranco/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package merlobranco.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import merlobranco.springframework.sfgpetclinic.services.VetService;
+
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 	
-	@GetMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@GetMapping({"", "/", "/index", "/index.html"})
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
