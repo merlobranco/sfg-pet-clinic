0b2440593d0c1f26cc8d02d745ced7a0d4fc7e31
Brais Cidras <braisomary@gmail.com>
2021-04-05 17:34:35 +0200

    Creating Specialty Map Service. Closes #31

---
+++ pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,7 @@
+package merlobranco.springframework.sfgpetclinic.services;
+
+import merlobranco.springframework.sfgpetclinic.models.Speciality;
+
+public interface SpecialtyService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,11 @@
+package merlobranco.springframework.sfgpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+
+import merlobranco.springframework.sfgpetclinic.models.Speciality;
+import merlobranco.springframework.sfgpetclinic.services.SpecialtyService;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
+
+}
