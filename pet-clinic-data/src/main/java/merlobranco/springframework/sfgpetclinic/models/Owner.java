68c667bf4a6b510c71191d7ce22bb71a55af81d3
Brais Cidras <braisomary@gmail.com>
2021-04-05 13:46:46 +0200

    Creating Pet Type, Pet, and Visit Entities. Closes #26

--- pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/models/Owner.java
+++ pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/models/Owner.java
@@ -1,7 +1,18 @@
 package merlobranco.springframework.sfgpetclinic.models;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
 	private static final long serialVersionUID = -4690876960225846075L;
+	
+	private Set<Pet> pets;
 
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 }
