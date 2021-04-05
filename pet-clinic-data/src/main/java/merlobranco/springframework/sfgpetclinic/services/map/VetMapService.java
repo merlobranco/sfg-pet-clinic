91f73ce3ce7fd0f85269808d7ba6b3bf53bce189
Brais Cidras <braisomary@gmail.com>
2021-04-05 17:46:25 +0200

    Adding Specialities to Vets on Startup. Closes #34

--- pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -2,9 +2,37 @@ package merlobranco.springframework.sfgpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
 
+import merlobranco.springframework.sfgpetclinic.models.Speciality;
 import merlobranco.springframework.sfgpetclinic.models.Vet;
+import merlobranco.springframework.sfgpetclinic.services.SpecialtyService;
 import merlobranco.springframework.sfgpetclinic.services.VetService;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+	
+	private final SpecialtyService specialtyService;
+
+	public VetMapService(SpecialtyService specialtyService) {
+		super();
+		this.specialtyService = specialtyService;
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		if (object == null) {
+			return null;
+		}
+		if (object.getSpecialities() == null) {
+			return super.save(object);
+		}
+		object.getSpecialities().forEach(speciality -> {
+            if(speciality.getId() == null) {
+                Speciality savedSpecialty = specialtyService.save(speciality);
+                speciality.setId(savedSpecialty.getId());
+            }
+        });
+		return super.save(object);
+	}
+	
+	
 }
