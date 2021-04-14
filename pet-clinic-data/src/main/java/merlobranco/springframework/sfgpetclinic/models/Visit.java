5358470f106582e8ee175d7322be1deec6895fec
Brais Cidras <braisomary@gmail.com>
2021-04-14 22:28:42 +0200

    Creating Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/models/Visit.java
+++ pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/models/Visit.java
@@ -2,12 +2,25 @@ package merlobranco.springframework.sfgpetclinic.models;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
 	private static final long serialVersionUID = 3978425626710110333L;
 
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 
 	public LocalDate getDate() {
