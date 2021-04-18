15f5e138d2c13d49ff750d1f0f2594914efd55f1
Brais Cidras <braisomary@gmail.com>
2021-04-18 19:40:53 +0200

    Refactoring for Project Lombok. Closes #51

--- pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/models/Person.java
+++ pet-clinic-data/src/main/java/merlobranco/springframework/sfgpetclinic/models/Person.java
@@ -3,10 +3,25 @@ package merlobranco.springframework.sfgpetclinic.models;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity {
 	
 	private static final long serialVersionUID = 4748273250606551747L;
+	
+	public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
 	@Column(name = "first_name")
 	private String firstName;
@@ -14,19 +29,4 @@ public class Person extends BaseEntity {
 	@Column(name = "last_name")
 	private String lastName;
 
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String fisrtName) {
-		this.firstName = fisrtName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
 }
